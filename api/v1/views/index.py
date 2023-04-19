7bed2a68b489a41533aada5d9604a976d8c04f3e
joseftango <4279@holbertonschool.com>
2023-04-19 20:30:51 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,19 +2,24 @@
 """module named index"""
 from api.v1.views import app_views
 from flask import jsonify
-from models import city, state, place, user, amenity, review, storage
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.user import User
+from models.amenity import Amenity
+from models.review import Review
+from models import storage
 
 
-my_classes= {'cities': city.City, 'states': state.State, 
-            'places': place.Place, 'users': user.User,
-            'amenities': amenity.Amenity, 'reviews': review.Review
-            }
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """get data in json format"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route("/stats", strict_slashes=False)
 def obj_counter():
