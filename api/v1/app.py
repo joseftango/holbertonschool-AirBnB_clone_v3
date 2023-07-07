f123761126b4fdeafb115b8a1244c7b3db8342be
joseftango <4279@holbertonschool.com>
2023-07-07 19:37:44 +0100

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,22 +3,19 @@
 from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
-
-
-@app.errorhandler(404)
-def invalid_route(e):
-    return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":
