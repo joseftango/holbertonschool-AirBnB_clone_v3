23b22b3642e6b73ac622f0967ca907ac0bba19ab
joseftango <4279@holbertonschool.com>
2025-05-09 13:46:45 +0100

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,26 +6,30 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET', 'POST'])
+@app_views.route('/users', methods=['GET'])
 def get_users():
     '''retrive list of all users'''
     if request.method == 'GET':
         users = list(map(lambda obj: obj.to_dict(),
                          storage.all(User).values()))
         return jsonify(users)
-    else:
-        data = request.get_json(silent=True)
 
-        if not data:
-            abort(400, description='Not a JSON')
-        if 'email' not in data:
-            abort(400, description='Missing email')
-        if 'password' not in data:
-            abort(400, description='Missing password')
 
-        new_user = User(**data)
-        new_user.save()
-        return jsonify(new_user.to_dict()), 201
+@app_views.route('/users/', methods=['POST'])
+def create_users():
+    '''creates a new user to acheive the POST method goal'''
+    data = request.get_json(silent=True)
+
+    if not data:
+        abort(400, description='Not a JSON')
+    if 'email' not in data:
+        abort(400, description='Missing email')
+    if 'password' not in data:
+        abort(400, description='Missing password')
+
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
